b4f637bf2c50f1409bc9f495ef7827f7458a04c4
Caitlin Cooling <ccooling97@gmail.com>
2018-09-25 19:28:33 +0100

    Add ability to save list of students to CSV

--- directory.rb
+++ directory.rb
@@ -1,4 +1,35 @@
 @students = [] #an empty array accessible to all methods
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when"9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 def ask_for_name
   puts "Please enter the name of a student"
   puts "To finish, enter stop"
@@ -42,33 +73,16 @@ def print_footer
     puts "Overall, we have #{@students.count} great students"
   end
 end
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when"9"
-    exit
-  else
-    puts "I don't know what you mean, try again"
+
+def save_students
+  file = File.open("student.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
   end
-end
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
+  file.close
+  puts "File saved."
 end
 
 interactive_menu
