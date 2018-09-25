d9deeffc123ac17727180a427897ac2791f0b739
Caitlin Cooling <ccooling97@gmail.com>
2018-09-25 16:53:28 +0100

    Finish the exercises

--- directory.rb
+++ directory.rb
@@ -1,24 +1,46 @@
+def ask_for_name
+  puts "Please enter the name of a student"
+  puts "To finish, enter stop"
+  name = gets.tr("\n", "").downcase
+end
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
   students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
+  while true do
+    name = ask_for_name
+    if name == "stop"
+      break
+    elsif name == ""
+      puts "Please enter your name"
+      name = gets.tr("\n", "")
+    end
+    puts "What cohort are they taking part in?"
+    cohort = gets.tr("\n", "")
+    if cohort == ""
+      cohort = "november"
+    end
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
-    name = gets.chomp
   end
   students
 end
 def print_header
-  puts "The student of Villians Academy"
-  puts "-------------"
+  puts "The student of Villians Academy".center(45)
+  puts "-------------".center(45)
 end
 def print(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+  sorted_students = students.group_by{ |student| student[:cohort] }
+  sorted_students.each do |cohort_month, students|
+    students.each do |student|
+      puts "#{student[:name]} is in the #{student[:cohort]} cohort"
+    end
+  end
 end
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student"
+  else
+    puts "Overall, we have #{students.count} great students"
+  end
 end
 
 students = input_students
