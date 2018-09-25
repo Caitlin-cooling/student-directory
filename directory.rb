fcc53b2541e533ead91ab902a69968cc68ac5d5c
Caitlin Cooling <ccooling97@gmail.com>
2018-09-25 19:48:40 +0100

    Add ability to load the studsents.csv

--- directory.rb
+++ directory.rb
@@ -3,6 +3,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 def show_students
@@ -18,6 +19,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when"9"
     exit
   else
@@ -75,7 +78,7 @@ def print_footer
 end
 
 def save_students
-  file = File.open("student.csv", "w")
+  file = File.open("students.csv", "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -83,6 +86,14 @@ def save_students
   end
   file.close
   puts "File saved."
+end
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 interactive_menu
